b79ce91b342f69777a43377cbe83ddf6816bd086
Curslo <richardkisivii@gmail.com>
2026-06-06 12:02:06 +0300

    Fix formatting and add missing semicolons in main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module.js'
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module.js";
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  
+  const app = await NestFactory.create(AppModule);
+
   // Enable CORS so your TanStack Start web app can fetch data from here
-  app.enableCors()
-  
-  await app.listen(4000)
-  console.log(`🚀 Server is running on: http://localhost:4000/graphql`)
+  app.enableCors();
+
+  await app.listen(4000);
+  console.log(`🚀 Server is running on: http://localhost:4000/graphql`);
 }
-bootstrap()
+bootstrap();
