647999716b81c43f45ac4d9c7eae08b6dbd7265d
Richard Kisivii <richardkisivii@gmail.com>
2026-06-19 22:46:37 +0300

    feat: remove tailwindcss plugin from ESLint configuration for React

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -2,7 +2,6 @@ import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginTailwind from "eslint-plugin-tailwindcss";
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import { config as baseConfig } from "./base.js";
@@ -17,7 +16,6 @@ export const config = [
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  ...pluginTailwind.configs["flat/recommended"],
   {
     languageOptions: {
       ...pluginReact.configs.flat.recommended.languageOptions,
@@ -30,20 +28,14 @@ export const config = [
   {
     plugins: {
       "react-hooks": pluginReactHooks,
-      tailwindcss: pluginTailwind,
     },
     settings: {
       react: { version: "detect" },
-      tailwindcss: {
-        // Ensure the linter inspects classes inside the internal UI package structures
-        callees: ["classnames", "clsx", "ctl", "cva", "cn"],
-      },
     },
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
       "react/react-in-jsx-scope": "off",
-      "tailwindcss/no-custom-classname": "off",
+      "react/prop-types": "off",
     },
   },
 ];
