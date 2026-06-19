a7c117347ece61a443ddbe2af743e0fcd2572c62
Richard Kisivii <richardkisivii@gmail.com>
2026-06-19 22:45:04 +0300

    feat: update ESLint and TypeScript configurations to enhance module support and ignore patterns

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,8 +1,15 @@
 import { config } from "@eastgate/eslint-config/react-internal";
 
 export default [
-  ...config,
   {
-    ignores: ["node_modules/**", "src/generated/**"],
+    ignores: [
+      "node_modules/**",
+      "src/generated/**",
+      "dist/**",
+      ".turbo/**",
+      "build/**",
+      ".next/**",
+    ],
   },
+  ...config,
 ];
