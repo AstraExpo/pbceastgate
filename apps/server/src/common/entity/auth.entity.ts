6d7e405adb5ceb4f7151dda2e228acdcc7d9efbd
Richard Kisivii <richardkisivii@gmail.com>
2026-08-02 08:32:45 +0300

    feat: add AuthResponse entity for JWT authentication response

---
+++ apps/server/src/common/entity/auth.entity.ts
@@ -0,0 +1,11 @@
+import { ObjectType, Field } from '@nestjs/graphql';
+import { User } from '../../user/models/user.model';
+
+@ObjectType()
+export class AuthResponse {
+  @Field(() => String, { description: 'The JWT access token' })
+  accessToken: string;
+
+  @Field(() => User, { description: 'The authenticated user profile' })
+  user: User;
+}
