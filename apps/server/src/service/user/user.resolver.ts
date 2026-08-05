61b8e61025b1ec25f3c1d1eb17f2bd35c1a74936
Richard Kisivii <richardkisivii@gmail.com>
2026-08-05 08:56:46 +0300

    style: standardize import statements and structure in UserResolver

---
+++ apps/server/src/service/user/user.resolver.ts
@@ -0,0 +1,42 @@
+import { Resolver, Query, Mutation, Args } from "@nestjs/graphql";
+import { UserService } from "./user.service";
+import { User } from "@/common/entity/user.entity";
+import { CreateUserInput } from "@/common/dto/user/create.dto";
+import { UpdateUserInput } from "@/common/dto/user/update.dto";
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  // --- QUERIES ---
+
+  @Query(() => User, { nullable: true, name: "userByEmail" })
+  async getUserByEmail(@Args("email") email: string) {
+    return this.userService.findByEmail(email);
+  }
+
+  @Query(() => User, { nullable: true, name: "userById" })
+  async getUserById(@Args("id") id: string) {
+    return this.userService.findById(id);
+  }
+
+  // --- MUTATIONS ---
+
+  @Mutation(() => User, { name: "createUser" })
+  async createUser(@Args("data") data: CreateUserInput) {
+    return this.userService.createUser(data);
+  }
+
+  @Mutation(() => User, { name: "updateUserDetails" })
+  async updateUserDetails(
+    @Args("id") id: string,
+    @Args("data") data: UpdateUserInput,
+  ) {
+    return this.userService.updateUserDetails(id, data);
+  }
+
+  @Mutation(() => User, { name: "deleteUser" })
+  async deleteUser(@Args("id") id: string) {
+    return this.userService.deleteUser(id);
+  }
+}
