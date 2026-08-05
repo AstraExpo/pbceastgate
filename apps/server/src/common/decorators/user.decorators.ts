595d526d6ecfe7af23f3e9bb016ce4bbf25a1c6b
Richard Kisivii <richardkisivii@gmail.com>
2026-08-05 08:53:47 +0300

    fix: reorder import statements for consistency and readability

--- apps/server/src/common/decorators/user.decorators.ts
+++ apps/server/src/common/decorators/user.decorators.ts
@@ -1,12 +1,13 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
+import { JwtPayload } from "@/service/auth/strategy/jwt.service";
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
 
 export const CurrentUser = createParamDecorator(
   (data: keyof JwtPayload | undefined, context: ExecutionContext) => {
     // 1. Translate the context for GraphQL
     const ctx = GqlExecutionContext.create(context);
     const request = ctx.getContext().req;
-    
+
     // 2. Grab the user object attached by the GqlAuthGuard
     const user = request.user;
 
