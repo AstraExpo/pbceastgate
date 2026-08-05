1bae5260fb43a30b019ac8983abc19584d8c5058
Richard Kisivii <richardkisivii@gmail.com>
2026-08-05 08:55:05 +0300

    style: clean up import statements in ServiceModule by removing file extensions

--- apps/server/src/service/service.module.ts
+++ apps/server/src/service/service.module.ts
@@ -1,12 +1,8 @@
-import { Module } from '@nestjs/common';
-import { SermonModule } from './sermon/sermon.module.js';
-import { MinistryModule } from './ministry/ministry.module.js';
+import { Module } from "@nestjs/common";
+import { SermonModule } from "./sermon/sermon.module";
+import { MinistryModule } from "./ministry/ministry.module";
 @Module({
-  imports: [
-    SermonModule,
-    MinistryModule
-  ],
-  providers: [
-  ],
+  imports: [SermonModule, MinistryModule],
+  providers: [],
 })
 export class ServiceModule {}
