f83f03f807746f6f713b084e4355e85ec37879f2
Richard Kisivii <richardkisivii@gmail.com>
2026-08-05 08:55:24 +0300

    style: standardize quotation marks in AuthResolver and format method arguments

--- apps/server/src/service/auth/auth.resolver.ts
+++ apps/server/src/service/auth/auth.resolver.ts
@@ -1,22 +1,29 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
-import { AuthService } from './auth.service.js';
+import { Resolver, Mutation, Args } from "@nestjs/graphql";
+import { AuthService } from "./auth.service.js";
+import { AuthResponse } from "@/common/entity/auth.entity.js";
+import { LoginInput } from "@/common/dto/auth/login.dto.js";
+import { RegisterInput } from "@/common/dto/auth/register.dto.js";
 
 @Resolver()
 export class AuthResolver {
   constructor(private authService: AuthService) {}
 
   @Mutation(() => AuthResponse)
-  async loginWithEmail(@Args('data') data: LoginInput) {
+  async loginWithEmail(@Args("data") data: LoginInput) {
     return this.authService.loginWithEmail(data.email, data.password);
   }
 
   @Mutation(() => AuthResponse)
-  async registerWithEmail(@Args('data') data: RegisterInput) {
-    return this.authService.registerWithEmail(data.email, data.password, data.firstName);
+  async registerWithEmail(@Args("data") data: RegisterInput) {
+    return this.authService.registerWithEmail(
+      data.email,
+      data.password,
+      data.firstName,
+    );
   }
 
   @Mutation(() => AuthResponse)
-  async loginWithProvider(@Args('firebaseToken') firebaseToken: string) {
+  async loginWithProvider(@Args("firebaseToken") firebaseToken: string) {
     return this.authService.loginWithFirebaseToken(firebaseToken);
   }
 }
