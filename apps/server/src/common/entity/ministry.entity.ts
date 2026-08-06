6c7118f72ab12752f1803f59f855f012a9c4e065
Richard Kisivii <richardkisivii@gmail.com>
2026-08-06 10:06:41 +0300

    style: update id and headId fields in Ministry entity to use string type

--- apps/server/src/common/entity/ministry.entity.ts
+++ apps/server/src/common/entity/ministry.entity.ts
@@ -2,7 +2,7 @@ import { Field, Int, ObjectType } from "@nestjs/graphql";
 
 @ObjectType()
 export class Ministry {
-  @Field(() => Int)
+  @Field(() => String)
   id!: number;
 
   @Field(() => String)
@@ -11,6 +11,6 @@ export class Ministry {
   @Field(() => String, { nullable: true })
   description!: string | null;
 
-  @Field(() => Int, { nullable: true })
+  @Field(() => String, { nullable: true })
   headId!: number | null;
 }
