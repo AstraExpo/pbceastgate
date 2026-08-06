76ff7f3c0e31d832c2da5423a9ea3ab8e4e55b04
Richard Kisivii <richardkisivii@gmail.com>
2026-08-06 10:07:06 +0300

    style: add AuthModule to ServiceModule imports

--- apps/server/src/service/service.module.ts
+++ apps/server/src/service/service.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { SermonModule } from "./sermon/sermon.module";
 import { MinistryModule } from "./ministry/ministry.module";
+import { AuthModule } from "./auth/auth.module";
 @Module({
-  imports: [SermonModule, MinistryModule],
+  imports: [SermonModule, MinistryModule, AuthModule],
   providers: [],
 })
 export class ServiceModule {}
