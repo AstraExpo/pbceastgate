737ccf722272e6d0745e810d157b63d408dc4a16
Richard Kisivii <richardkisivii@gmail.com>
2026-08-07 06:42:46 +0300

    feat: enhance FirebaseAuthGuard with public access handling and user validation

--- apps/server/src/service/auth/guard/firebase-auth.guard.ts
+++ apps/server/src/service/auth/guard/firebase-auth.guard.ts
@@ -5,18 +5,34 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { GqlExecutionContext } from "@nestjs/graphql";
 import { getAuth } from "firebase-admin/auth";
+import { IS_PUBLIC_KEY } from "../decorators/public.decorators";
 
 @Injectable()
 export class FirebaseAuthGuard implements CanActivate {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
     const ctx = GqlExecutionContext.create(context);
     const req = ctx.getContext().req;
 
     const authHeader = req.headers["authorization"];
+
+    if (isPublic && (!authHeader || !authHeader.startsWith("Bearer "))) {
+      req.user = null;
+      return true;
+    }
+
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       throw new UnauthorizedException("No authorization token provided");
     }
@@ -26,12 +42,15 @@ export class FirebaseAuthGuard implements CanActivate {
     try {
       const decodedToken = await getAuth().verifyIdToken(token);
 
-      const dbUser = await this.prisma.user.findUnique({
-        where: { id: decodedToken.uid },
+      const dbUser = await this.prismaService.user.findUnique({
+        where: { firebaseUid: decodedToken.uid },
       });
 
       if (!dbUser) {
-        throw new UnauthorizedException("User record not found in database.");
+        await getAuth().deleteUser(decodedToken.uid);
+        throw new UnauthorizedException(
+          "Access denied. Rogue account removed.",
+        );
       }
 
       req.user = dbUser;
