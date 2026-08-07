709c0cc23af56b2f3266a7ba5db0e03337192c36
Richard Kisivii <richardkisivii@gmail.com>
2026-08-07 06:41:34 +0300

    feat: enhance CreateUserInput DTO with additional fields and validations

--- apps/server/src/common/dto/user/create.dto.ts
+++ apps/server/src/common/dto/user/create.dto.ts
@@ -1,10 +1,21 @@
+import {
+  MembershipStatus,
+  SystemRole,
+} from "@/common/graphql/generated/apollo.types";
 import { InputType, Field } from "@nestjs/graphql";
-import { IsEmail, IsNotEmpty, IsString, IsOptional } from "class-validator";
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+  IsEnum,
+} from "class-validator";
 
 @InputType()
 export class CreateUserInput {
   @Field(() => String)
   @IsEmail()
+  @IsNotEmpty()
   email!: string;
 
   @Field(() => String)
@@ -12,8 +23,23 @@ export class CreateUserInput {
   @IsNotEmpty()
   name!: string;
 
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  firebaseUid!: string;
+
   @Field(() => String, { nullable: true })
   @IsString()
   @IsOptional()
-  password?: string;
+  image?: string;
+
+  @Field(() => String, { defaultValue: SystemRole.User, nullable: true })
+  @IsEnum(SystemRole)
+  @IsOptional()
+  systemRole?: SystemRole;
+
+  @Field(() => String, { defaultValue: MembershipStatus.Guest, nullable: true })
+  @IsEnum(MembershipStatus)
+  @IsOptional()
+  membershipStatus?: MembershipStatus;
 }
