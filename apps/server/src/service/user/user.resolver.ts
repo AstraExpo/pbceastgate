d460f62e5b3325c458fd05c0a3fa287ba6fc1959
Richard Kisivii <richardkisivii@gmail.com>
2026-08-07 06:43:38 +0300

    feat: refactor UserResolver for improved user queries and mutations

--- apps/server/src/service/user/user.resolver.ts
+++ apps/server/src/service/user/user.resolver.ts
@@ -3,6 +3,10 @@ import { UserService } from "./user.service";
 import { User } from "@/common/entity/user.entity";
 import { CreateUserInput } from "@/common/dto/user/create.dto";
 import { UpdateUserInput } from "@/common/dto/user/update.dto";
+import {
+  MembershipStatus,
+  SystemRole,
+} from "@/common/graphql/generated/apollo.types";
 
 @Resolver(() => User)
 export class UserResolver {
@@ -10,38 +14,72 @@ export class UserResolver {
 
   // --- QUERIES ---
 
-  @Query(() => User, { nullable: true, name: "userByEmail" })
-  async getUserByEmail(@Args("email") email: string) {
-    return this.userService.findByEmail(email);
+  @Query(() => [User])
+  async getUsers() {
+    return this.userService.getUsers();
   }
 
-  @Query(() => User, { nullable: true, name: "userById" })
-  async getUserById(@Args("id") id: string) {
-    return this.userService.findById(id);
+  @Query(() => [User])
+  async getAdminUsers(@Args("adminRole") adminRole: SystemRole.Admin) {
+    return this.userService.getAdminUsers(adminRole);
+  }
+
+  @Query(() => [User])
+  async getEditorUsers(@Args("editorRole") editorRole: SystemRole.Editor) {
+    return this.userService.getEditorUsers(editorRole);
+  }
+
+  @Query(() => [User])
+  async getNormalUsers(@Args("userRole") userRole: SystemRole.User) {
+    return this.userService.getNormalUsers(userRole);
+  }
+
+  @Query(() => [User])
+  async getMemberUsers(
+    @Args("memberStatus") memberStatus: MembershipStatus.Member,
+  ) {
+    return this.userService.getMemberUsers(memberStatus);
+  }
+
+  @Query(() => [User])
+  async getGuestUsers(
+    @Args("guestStatus") guestStatus: MembershipStatus.Guest,
+  ) {
+    return this.userService.getGuestUsers(guestStatus);
+  }
+
+  @Query(() => User)
+  async getUserByEmail(@Args("email") email: string) {
+    return this.userService.getByEmail(email);
+  }
+
+  @Query(() => User)
+  async getUserById(@Args("userId") userId: string) {
+    return this.userService.getById(userId);
   }
 
   @Query(() => User)
   async getUserByFirebaseUid(@Args("firebaseUid") firebaseUid: string) {
-    return this.userService.findByFirebaseUid(firebaseUid);
+    return this.userService.getByFirebaseUid(firebaseUid);
   }
 
   // --- MUTATIONS ---
 
-  @Mutation(() => User, { name: "createUser" })
-  async createUser(@Args("data") data: CreateUserInput) {
-    return this.userService.createUser(data);
+  @Mutation(() => User)
+  async createUser(@Args("input") input: CreateUserInput) {
+    return this.userService.createUser(input);
   }
 
-  @Mutation(() => User, { name: "updateUserDetails" })
-  async updateUserDetails(
-    @Args("id") id: string,
-    @Args("data") data: UpdateUserInput,
+  @Mutation(() => User)
+  async updateUser(
+    @Args("userId") userId: string,
+    @Args("input") input: UpdateUserInput,
   ) {
-    return this.userService.updateUserDetails(id, data);
+    return this.userService.updateUserDetails(userId, input);
   }
 
-  @Mutation(() => User, { name: "deleteUser" })
-  async deleteUser(@Args("id") id: string) {
-    return this.userService.deleteUser(id);
+  @Mutation(() => User)
+  async deleteUser(@Args("userId") userId: string) {
+    return this.userService.deleteUser(userId);
   }
 }
