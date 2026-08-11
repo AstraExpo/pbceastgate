fca924b93cf9fdae94ed6b477f0d6594ff452235
Richard Kisivii <richardkisivii@gmail.com>
2026-08-11 14:42:51 +0300

    feat: refactor Firebase initialization and CORS configuration in main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,36 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { initializeApp, cert } from "firebase-admin/app";
-import { ConfigService } from "@nestjs/config";
-import { EnvironmentVariables } from "./common/config/env.validate"; // Adjust path if needed
+import { AppConfigService } from "./common/config/app-config.service";
+import { initializeFirebaseAdmin } from "./common/firebase/firebase.provider";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(AppConfigService);
 
-  const configService =
-    app.get<ConfigService<EnvironmentVariables, true>>(ConfigService);
+  initializeFirebaseAdmin(configService);
 
-  const serviceAccount = {
-    projectId: configService.get("FIREBASE_PROJECT_ID", { infer: true }),
-    clientEmail: configService.get("FIREBASE_CLIENT_EMAIL", { infer: true }),
-    privateKey: configService.get("FIREBASE_PRIVATE_KEY", { infer: true }),
-  };
-
-  initializeApp({
-    credential: cert(serviceAccount),
-  });
-
-  const allowedOriginsString = configService.get("ALLOWED_ORIGINS", {
-    infer: true,
-  });
-  const allowedOrigins = allowedOriginsString
-    ? allowedOriginsString.split(",")
-    : [];
-
-  app.enableCors({
-    origin: allowedOrigins,
-    credentials: true,
-  });
+  app.enableCors({ origin: configService.allowedOrigins, credentials: true });
 
   await app.listen(4000);
   console.log(`🚀 Server is running on: http://localhost:4000/graphql`);
