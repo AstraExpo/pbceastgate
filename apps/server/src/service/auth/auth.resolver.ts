47a2985aabe77aac749117bcdcc1c68c50ebc414
Richard Kisivii <richardkisivii@gmail.com>
2026-08-12 08:39:41 +0300

    feat: refactor authentication methods to use CurrentFirebaseUser decorator and streamline user signup process

--- apps/server/src/service/auth/auth.resolver.ts
+++ apps/server/src/service/auth/auth.resolver.ts
@@ -1,10 +1,12 @@
-import { Resolver, Mutation, Args } from "@nestjs/graphql";
+import { Resolver, Mutation } from "@nestjs/graphql";
 import { AuthService } from "./auth.service";
 import { User } from "@/common/entity/user.entity";
 import { RequireRoles } from "./decorators/roles.decorators";
 import { SystemRole } from "@/common/graphql/generated/apollo.types";
 import { Public } from "./decorators/public.decorators";
 import { CurrentUser } from "./decorators/user.decorators";
+import { CurrentFirebaseUser } from "./decorators/firebase-user.decorator";
+import { type DecodedIdToken } from "firebase-admin/auth";
 
 @Resolver()
 export class AuthResolver {
@@ -17,13 +19,13 @@ export class AuthResolver {
   }
 
   @Mutation(() => User)
-  async authenticateUsers(@CurrentUser() user: User) {
+  async authenticateCongregant(@CurrentUser() user: User) {
     return user;
   }
 
   @Public()
   @Mutation(() => User)
-  async signUpUser(@Args("firebaseToken") firebaseToken: string) {
-    return this.authService.signUpNewUser(firebaseToken);
+  async signUpCongregant(@CurrentFirebaseUser() firebaseUser: DecodedIdToken) {
+    return this.authService.signUpNewCongregant(firebaseUser);
   }
 }
--- apps/server/src/service/auth/auth.service.ts
+++ apps/server/src/service/auth/auth.service.ts
@@ -1,9 +1,5 @@
-import {
-  ConflictException,
-  Injectable,
-  UnauthorizedException,
-} from "@nestjs/common";
-import { getAuth } from "firebase-admin/auth";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { type DecodedIdToken } from "firebase-admin/auth";
 import { UserService } from "../user/user.service";
 import { CreateUserInput } from "@/common/dto/user/create.dto";
 import {
@@ -15,38 +11,23 @@ import {
 export class AuthService {
   constructor(private userService: UserService) {}
 
-  async signUpNewUser(firebaseToken: string) {
-    try {
-      const decodedToken = await getAuth().verifyIdToken(firebaseToken);
-      const { email, name, picture, uid } = decodedToken;
+  async signUpNewCongregant(firebaseUser: DecodedIdToken) {
+    const { email, name, picture, uid } = firebaseUser;
 
-      if (!email)
-        throw new UnauthorizedException("No email provided by provider");
+    if (!email)
+      throw new UnauthorizedException("No email provided by provider");
 
-      const existingUser = await this.userService.getByFirebaseUid(uid);
+    const createUserInput: CreateUserInput = {
+      email: email,
+      name: name || email.split("@")[0],
+      firebaseUid: uid,
+      image: picture,
+      systemRole: SystemRole.User,
+      membershipStatus: MembershipStatus.Guest,
+    };
 
-      if (existingUser) {
-        throw new ConflictException(
-          "An account with this email already exists. Please sign in instead.",
-        );
-      }
+    const newUser = await this.userService.createUser(createUserInput);
 
-      const createUserInput: CreateUserInput = {
-        email: email,
-        name: name || email.split("@")[0],
-        firebaseUid: uid,
-        image: picture,
-        systemRole: SystemRole.User,
-        membershipStatus: MembershipStatus.Guest,
-      };
-
-      const newUser = await this.userService.createUser(createUserInput);
-
-      return newUser;
-    } catch (error) {
-      if (error instanceof ConflictException) throw error;
-
-      throw new UnauthorizedException(`Sign up failed: ${error}`);
-    }
+    return newUser;
   }
 }
