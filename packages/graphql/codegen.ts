b40bdaff02cb3f177e52de0f2642101740783738
Richard Kisivii <richardkisivii@gmail.com>
2026-08-12 15:21:11 +0300

    feat: add GraphQL code generation configuration

---
+++ packages/graphql/codegen.ts
@@ -0,0 +1,69 @@
+import { join } from "node:path";
+import process from "node:process";
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const rootDir = process.cwd();
+
+const apolloConfig = {
+  client: join(
+    rootDir,
+    "../../apps/client/src/graphql/generated/client.graphql.ts",
+  ),
+  admin: join(
+    rootDir,
+    "../../apps/admin/src/graphql/generated/admin.graphql.ts",
+  ),
+  server: join(
+    rootDir,
+    "../../apps/server/src/common/graphql/generated/server.types.ts",
+  ),
+};
+
+const schema = join(rootDir, "../../apps/server/src/common/graphql/schema.gql");
+
+const scalars = {
+  Date: "string",
+  DateTime: "string",
+  JSON: "{ [key: string]: unknown }",
+  UUID: "string",
+};
+
+const config: CodegenConfig = {
+  schema,
+  generates: {
+    [apolloConfig.client]: {
+      documents: join(
+        rootDir,
+        "../../apps/client/src/graphql/operations/**/*.graphql",
+      ),
+      plugins: ["typescript", "typescript-operations", "typed-document-node"],
+      config: {
+        scalars,
+        nonOptionalTypename: true,
+        documentMode: "documentNode",
+      },
+    },
+
+    [apolloConfig.admin]: {
+      documents: join(
+        rootDir,
+        "../../apps/admin/src/graphql/operations/**/*.graphql",
+      ),
+      plugins: ["typescript", "typescript-operations", "typed-document-node"],
+      config: {
+        scalars,
+        nonOptionalTypename: true,
+        documentMode: "documentNode",
+      },
+    },
+    [apolloConfig.server]: {
+      plugins: ["typescript", "typescript-resolvers"],
+      config: {
+        scalars,
+        useIndexSignature: true,
+      },
+    },
+  },
+};
+
+export default config;
