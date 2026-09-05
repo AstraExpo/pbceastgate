4b09c1316b9a4ecaae22a3b720e98d8defc2dda5
Richard Kisivii <richardkisivii@gmail.com>
2026-09-05 09:24:21 +0300

    feat(auth): enhance authentication routing and add new admin and protected routes

--- apps/client/src/app/_auth/route.tsx
+++ apps/client/src/app/_auth/route.tsx
@@ -1,13 +1,18 @@
-import { AuthStatus } from "@/graphql";
+import { AuthStatus, SystemRole } from "@/graphql";
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_auth")({
   beforeLoad: ({ context }) => {
-    if (context.auth.status === AuthStatus.Authenticated) {
-      throw redirect({
-        to: "/home",
-      });
+    if (context.auth.status !== AuthStatus.Authenticated) return;
+    if (
+      (context.auth.status === AuthStatus.Authenticated &&
+        context.auth.user.systemRole == SystemRole.Admin) ||
+      SystemRole.Editor ||
+      SystemRole.System
+    ) {
+      throw redirect({ to: "/dashboard" });
     }
+    throw redirect({ to: "/home" });
   },
   component: AuthLayout,
 });
